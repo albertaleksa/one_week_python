438851bec4440df829c51f36b3256ff4788054bc
Albert Aleksa <albert.aleksa.by@gmail.com>
2022-09-10 13:36:47 -0500

    Binary Search Tree (BST)

---
+++ data_structures/trees/binary_search_tree_implementation.py
@@ -0,0 +1,218 @@
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def __str__(self):
+        return str(self.traverse(self.root))
+
+    def traverse(self, node):
+        if node:
+            tree = {"value": node.value if node else "null",
+                    "left": self.traverse(node.left) if node.left else "null",
+                    "right": self.traverse(node.right) if node.right else "null"}
+            return tree
+
+    def insert(self, value):
+        # check input
+        node = Node(value)
+        if not self.root:
+            self.root = node
+            return
+        cur_node = self.root
+        while cur_node:
+            # left
+            if value < cur_node.value:
+                if not cur_node.left:
+                    cur_node.left = node
+                    return
+                cur_node = cur_node.left
+            # right
+            elif value > cur_node.value:
+                if not cur_node.right:
+                    cur_node.right = node
+                    return
+                cur_node = cur_node.right
+            # duplicate
+            else:
+                print("Duplicate value")
+                return
+
+    def lookup(self, value):
+        if not self.root:
+            print("Tree is empty")
+        cur_node = self.root
+        while cur_node:
+            if value == cur_node.value:
+                return cur_node
+            if value < cur_node.value:
+                cur_node = cur_node.left
+            else:
+                cur_node = cur_node.right
+        return None
+
+    def remove(self, value):
+        # check if tree is empty
+        if not self.root:
+            print("tree is empty")
+            return
+        # find current node for deleting and his parent
+        cur_node, parent = self._search(value)
+        # check cur_node and parent for None
+        if cur_node:
+            print(cur_node.value, end=" ")
+        if parent:
+            print(parent.value)
+        if not cur_node:
+            print(f"node {value} is not in the tree")
+            return
+        if not parent:
+            print(f"node {value} is root node")
+
+        # if cur_node is a leaf
+        if self.is_leaf(cur_node):
+            # check if cur_node is root
+            if not parent:
+                self.root = None
+                print(f"node {value} is root node")
+            # cur_node isn't a root node
+            else:
+                # delete cur_node
+                self._delete(cur_node, parent)
+            print(f"node {value} is deleted")
+
+        # if cur_node has only one child
+        elif self.has_one_child(cur_node):
+            self.bypass(cur_node, parent)
+            print(f"node {value} is deleted")
+        # if cur_node has 2 children
+        else:
+            successor, suc_parent = self._find_successor(cur_node)
+            self._replace(cur_node, parent, successor, suc_parent)
+            print(f"node {value} is deleted")
+
+    def _search(self, value):
+        if not self.root:
+            return None, None
+        cur_node = self.root
+        parent = None
+        while cur_node:
+            if value == cur_node.value:
+                return cur_node, parent
+            else:
+                parent = cur_node
+                if value < cur_node.value:
+                    cur_node = cur_node.left
+                else:
+                    cur_node = cur_node.right
+        return None, None
+
+    def is_leaf(self, node):
+        if node.left or node.right:
+            return False
+        return True
+
+    def _delete(self, cur_node, parent):
+        if cur_node and parent:
+            # cur_node is a left child
+            if parent.left == cur_node:
+                parent.left = None
+            # cur_node is a right child
+            else:
+                parent.right = None
+
+    def has_one_child(self, node):
+        if node.left and node.right:
+            return False
+        if self.is_leaf(node):
+            return False
+        return True
+
+    def bypass(self, cur_node, parent):
+        # current node has left child
+        if cur_node.left:
+            # save left child of cur_node to new_node
+            new_node = cur_node.left
+        # current node has right child
+        else:
+            # save right child of cur_node to new_node
+            new_node = cur_node.right
+        # then new_node will contain link to cur_node's child
+        # check if cur_node ia a root
+        if not parent:
+                self.root = new_node
+        else:
+            # current node is left child
+            if parent.left == cur_node:
+                parent.left = new_node
+            # current node is right child
+            else:
+                parent.right = new_node
+
+    def _find_successor(self, node):
+        successor = node.right
+        suc_parent = node
+        while successor.left:
+            suc_parent = successor
+            successor = successor.left
+        return successor, suc_parent
+
+    def _replace(self, cur_node, parent, successor, suc_parent):
+        print(f"cur_node = {cur_node.value} parent = {parent.value} successor = {successor.value}"
+              f" suc_parent = {suc_parent.value}")
+        # link cur_node's left child instead of successor's left child
+        successor.left = cur_node.left
+        # if cur_node's right child is not a successor
+        if cur_node.right != successor:
+            # then link cur_node's right child instead of successor's right child
+            successor.right = cur_node.right
+            # delete link from successor's parent to successor (left child)
+            suc_parent.left = None
+        # check if cur_node is a root
+        if not parent:
+            self.root = successor
+        # link from parent to successor
+        if parent.left == cur_node:
+            parent.left = successor
+        else:
+            parent.right = successor
+        print(f"parent = {parent.value} parent.left = {parent.left.value} parent.right = {parent.right.value}")
+        print(f"parent.left.left = {parent.left.left.value}")
+
+
+my_tree = BST()
+print(my_tree)
+my_tree.insert(9)
+print(my_tree)
+my_tree.insert(4)
+print(my_tree)
+my_tree.insert(6)
+my_tree.insert(20)
+my_tree.insert(170)
+my_tree.insert(15)
+my_tree.insert(1)
+my_tree.insert(5)
+my_tree.insert(7)
+print(my_tree)
+
+print(my_tree.lookup(9))
+print(my_tree.lookup(1))
+print(my_tree.lookup(20))
+print(my_tree.lookup(170))
+print(my_tree.lookup(15))
+print(my_tree.lookup(8))
+
+# my_tree.remove(1)
+# print(my_tree)
+
+my_tree.remove(4)
+# print(my_tree)
