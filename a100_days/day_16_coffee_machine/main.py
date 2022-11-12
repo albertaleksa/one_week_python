b4f02e34326c1b14fb73daf73f09966e5525f919
Albert Aleksa <albert.aleksa.by@gmail.com>
2022-11-12 13:26:46 -0600

    quiz-game-start

--- a100_days/day_16_coffee_machine/main.py
+++ a100_days/day_16_coffee_machine/main.py
@@ -3,9 +3,9 @@ from a100_days.day_16_coffee_machine.coffee_maker import CoffeeMaker
 from a100_days.day_16_coffee_machine.money_machine import MoneyMachine
 
 if __name__ == "__main__":
+    money_machine = MoneyMachine()
     coffee_maker = CoffeeMaker()
     menu = Menu()
-    money_machine = MoneyMachine()
     while True:
         user_input = input(f"​What would you like? ({menu.get_items()}): ​")
         if user_input == "report":
@@ -16,8 +16,7 @@ if __name__ == "__main__":
         elif user_input in ("espresso", "latte", "cappuccino"):
             drink = menu.find_drink(user_input)
 
-            if coffee_maker.is_resource_sufficient(drink):
-                if money_machine.make_payment(drink.cost):
-                    coffee_maker.make_coffee(drink)
+            if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
         else:
             print("I don't know this command!")
