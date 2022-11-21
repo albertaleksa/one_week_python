b435926e5750c6583a05c6941d5ab784af39034d
Albert Aleksa <albert.aleksa.by@gmail.com>
2022-11-20 21:34:54 -0600

    day 18

---
+++ a100_days/day_20_snake_game/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Courier', 24, 'normal')
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.goto(x=0, y=270)
+        self.hideturtle()
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.update_scoreboard()
+
