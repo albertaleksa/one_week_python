a58463eca22b217631c7f70d4d57d1c1180faad1
Albert Aleksa <albert.aleksa.by@gmail.com>
2022-11-23 16:38:18 -0600

    day 20. Snake game. Add functionality to store high_score into the file

--- a100_days/day_20_snake_game/scoreboard.py
+++ a100_days/day_20_snake_game/scoreboard.py
@@ -11,17 +11,33 @@ class Scoreboard(Turtle):
         self.goto(x=0, y=270)
         self.hideturtle()
         self.score = 0
+        self.high_score = self.read_high_score()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_high_score()
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.update_scoreboard()
 
+    def read_high_score(self):
+        with open("data.txt") as data:
+            high_score = int(data.read())
+        return high_score
+
+    def write_high_score(self):
+        with open("data.txt", "w") as data:
+            data.write(f"{self.high_score}")
