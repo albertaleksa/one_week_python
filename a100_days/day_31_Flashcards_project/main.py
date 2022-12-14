1c932a5d95660f0a26b2e2f846f5edc84e87b906
Albert Aleksa <albert.aleksa.by@gmail.com>
2022-12-14 13:11:47 -0600

    Day 31 Flash Card App. App for learning words. Add UI using tkinter

--- a100_days/day_31_Flashcards_project/main.py
+++ a100_days/day_31_Flashcards_project/main.py
@@ -10,16 +10,17 @@ def start():
     window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
     # Images
-    card_front_img = PhotoImage(file="./images/card_front.png")
-    card_back_img = PhotoImage(file="./images/card_back.png")
-    wrong_img = PhotoImage(file="./images/wrong.png")
-    right_img = PhotoImage(file="./images/right.png")
+    card_front_img = PhotoImage(file="images/card_front.png")
+    card_back_img = PhotoImage(file="images/card_back.png")
+    wrong_img = PhotoImage(file="images/wrong.png")
+    right_img = PhotoImage(file="images/right.png")
 
     # Canvas
-    canvas = Canvas(width=800, height=526, highlightthickness=0)
+    canvas = Canvas(width=800, height=526)
     canvas.create_image(400, 263, image=card_front_img)
-    title_text = canvas.create_text(400, 150, text="Title", fill="black", font=(FONT_NAME, 40, "italic"))
-    word_text = canvas.create_text(400, 263, text="Word", fill="black", font=(FONT_NAME, 60, "bold"))
+    canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+    title_text = canvas.create_text(400, 150, text="Title", font=(FONT_NAME, 40, "italic"))
+    word_text = canvas.create_text(400, 263, text="Word", font=(FONT_NAME, 60, "bold"))
     # canvas.itemconfig(timer_text, text="00:00")
 
     canvas.grid(row=0, column=0, columnspan=2)
