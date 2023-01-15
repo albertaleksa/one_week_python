81f2033ab240fa86b43a3980356b44a97ed7a1bb
Albert Aleksa <albert.aleksa.by@gmail.com>
2023-01-15 13:08:44 -0600

    day_32

--- a100_days/day_32_smtp/main.py
+++ a100_days/day_32_smtp/main.py
@@ -1,13 +1,16 @@
 import smtplib
+import os
+from dotenv import load_dotenv
 
-my_email = "albert.aleksa.by@gmail.com"
-password = "cxmvdmkzhurxlakm"
+load_dotenv(override=True)
+MY_EMAIL = os.environ['MY_EMAIL']
+MY_PASSWORD = os.environ['MY_EMAIL_PASSWORD']
 
 with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
     connection.starttls()
-    connection.login(user=my_email, password=password)
+    connection.login(user=MY_EMAIL, password=MY_PASSWORD)
     connection.sendmail(
-        from_addr=my_email,
+        from_addr=MY_EMAIL,
         to_addrs="dread66620@gmail.com",
         msg="Subject:Test email\n\nThis is a test email from Python app."
     )
